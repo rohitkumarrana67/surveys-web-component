069758cdc2b70020bd091fb628f10f498c219109
Vaibhav Srivastava <vaibhav@engagedly.com>
2021-04-17 22:11:45 +0530

    Added subnav

--- public/common/eng_subnav.js
+++ public/common/eng_subnav.js
@@ -7,17 +7,35 @@ class EngSubnav extends HTMLElement{
                       </ul>
                      </nav>`
     this._navItems;
-    this._navItemsIDs
+    this._activeItem;
   }
 
   connectedCallback(){
     this._navItems = this.getAttribute('navItems');
-    this._navItemsIDs = this.getAttribute('navItemsIDs');
+    this._activeItem = this.getAttribute('activeItem') || 'overview';
+    this.innerHTML = this.template;
     this.populateNavItems();
+    this.addEventListeners();
   }
 
   populateNavItems(){
+    let nav_items = this._navItems.split(',');
+    for(let item of nav_items){
+      item = item.trim();
+      $(this).find('ul').append(`<li class="nav-item ${item.replace(' ', '_').toLowerCase()}"><a class="nav-link" href="${window.location.pathname}#${item.replace(' ', '_').toLowerCase()}">${item}</a></li>`);
+    }
+    $(this).find(`.${this._activeItem}`).addClass('active');
+  }
 
+  addEventListeners(){
+    $(this).find('li.nav-item').on('click', this.changeActiveItem.bind(this));
+  }
+
+  changeActiveItem(e) {
+    target = $(e.currentTarget);
+    debugger
+    $(this).find('li.active').removeClass('active');
+    target.addClass('active');
   }
 
 }
--- public/initializer/survey_container.js
+++ public/initializer/survey_container.js
@@ -3,22 +3,60 @@ class SurveyContainer extends HTMLElement{
   constructor(){
     super();
     this.template;
+    if(!window.customElements.get("eng_subnav")){
+      $.getScript('http://localhost:4001/common/eng_subnav.js', ()=>{})
+    }
   }
 
   connectedCallback(){
-    fetch('http://localhost:4002/survey/survey_container').then((response)=>{
+    this.type = this.getAttribute('type') || 'overview'
+    fetch(`http://localhost:4002/survey/survey_container?type=${this.type}`).then((response)=>{
       return response.text();
     }).then((response)=>{
       this.template = response;
       this.innerHTML = this.template;
-      this.renderOverView()
+      this.render();
     })
   }
 
-  renderOverView(){
+  render(){
+    switch (this.type) {
+      case 'overview':
+        this.renderOverview();
+        break;
+      case 'dashboard':
+        this.renderDashboard();
+        break;
+      case 'all_surveys':
+        break;
+      case 'template_library':
+        break;
+      case 'reports':
+        break;
+      default:
+        break;
+    }
+  }
+
+  renderOverview(){
     $.getScript('http://localhost:4001/overview/overview.js', ()=>{
       $(this).find('#survey-main-content').html('<survey-overview></survey-overview>')
     })
+  }
+
+  renderDashboard() {
+    $(this).find('#survey-main-content').html("This is dashboard");
+  }
+
+  static get observedAttributes() {
+    return ['type'];
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if(attr == 'type' && oldValue !== newValue){
+      this.type = newValue;
+      this.render();
+    }
   }
 }
 
